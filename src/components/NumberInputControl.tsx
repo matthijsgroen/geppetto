e15bb449508ef7bea5c224c91f1ad7e591f15319
Matthijs Groen <matthijs.groen@gmail.com>
2021-02-08 22:36:17 +0100

    Improve number input

--- src/components/NumberInputControl.tsx
+++ src/components/NumberInputControl.tsx
@@ -4,15 +4,19 @@ import styled from "styled-components";
 const Control = styled.div`
   padding: 0.5rem 0;
   border-bottom: 1px solid ${({ theme }) => theme.colors.text};
+  display: flex;
 `;
 
 const ControlLabel = styled.p`
   color: ${({ theme }) => theme.colors.text};
   margin: 0;
+  flex: 1;
 `;
 
-const NumberInput = styled.input.attrs({ type: "number" })`
+const NumberInput = styled.input.attrs({ type: "number", size: 6 })`
   text-align: right;
+  width: 6em;
+  flex: 0;
 `;
 
 interface SliderControlProps {
