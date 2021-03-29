e86d37dfe24ea83027b73bd7623434a8684f7003
Matthijs Groen <matthijs.groen@gmail.com>
2021-03-29 22:02:07 +0200

    Initial website test

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,10 +1,10 @@
-import { createPlayer, prepareAnimation } from "geppetto-player";
+import { setupWebGL, prepareAnimation } from "geppetto-player";
 import image from "url:./assets/landscape.png";
 import animation from "./assets/landscape.json";
 
 const canvas = document.getElementById("theatre") as HTMLCanvasElement;
 
-const player = createPlayer(canvas);
+const player = setupWebGL(canvas);
 
 const loadTexture = async (url: string): Promise<HTMLImageElement> =>
   new Promise((resolve) => {
@@ -17,9 +17,52 @@ const loadTexture = async (url: string): Promise<HTMLImageElement> =>
 const start = async () => {
   const img = await loadTexture(image);
   const preparedAnimation = prepareAnimation(animation);
-  const backgroundAnimation = player.addAnimation(preparedAnimation, img);
+  const backgroundAnimation = player.addAnimation(preparedAnimation, img, 0, {
+    zoom: 2.0,
+  });
 
-  backgroundAnimation.render();
+  const box = canvas.getBoundingClientRect();
+  canvas.width = box.width * window.devicePixelRatio;
+  canvas.height = box.height * window.devicePixelRatio;
+
+  backgroundAnimation.startTrack("Waterrad");
+  backgroundAnimation.startTrack("Waterrad2");
+  backgroundAnimation.startTrack("Rook");
+
+  const [smokeButton, wheelButton] = document.querySelectorAll("button");
+
+  let smokePlaying = true;
+  let wheelPlaying = true;
+  wheelButton.addEventListener("click", () => {
+    wheelPlaying = !wheelPlaying;
+    wheelButton.innerText = wheelPlaying ? "Stop wheel" : "Start wheel";
+    if (wheelPlaying) {
+      backgroundAnimation.startTrack("Waterrad");
+      backgroundAnimation.startTrack("Waterrad2");
+    } else {
+      backgroundAnimation.stopTrack("Waterrad");
+      backgroundAnimation.stopTrack("Waterrad2");
+    }
+  });
+
+  smokeButton.addEventListener("click", () => {
+    smokePlaying = !smokePlaying;
+    smokeButton.innerText = smokePlaying ? "Stop smoke" : "Start smoke";
+    if (smokePlaying) {
+      backgroundAnimation.startTrack("Rook");
+    } else {
+      backgroundAnimation.stopTrack("Rook");
+    }
+  });
+
+  const renderFrame = () => {
+    player.render();
+    backgroundAnimation.render();
+
+    window.requestAnimationFrame(renderFrame);
+  };
+
+  window.requestAnimationFrame(renderFrame);
 };
 
 start();
