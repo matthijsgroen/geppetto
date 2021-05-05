182008cb3e173e99d2d299d9a12348ca0143b118
Matthijs Groen <matthijs.groen@gmail.com>
2021-05-05 13:01:22 +0200

    Implement name compression

--- src/lib/compressFile.ts
+++ src/lib/compressFile.ts
@@ -1,4 +1,33 @@
+import {
+  getLayerNames,
+  getVectorNames,
+  renameLayer,
+  renameVector,
+} from "./definitionHelpers";
 import { ImageDefinition } from "./types";
 
-export const compressFile = (imageDef: ImageDefinition): ImageDefinition =>
-  imageDef;
+const NAME_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const nameForIndex = (number: number): string => {
+  if (number >= NAME_CHARS.length) {
+    return `${NAME_CHARS[number % NAME_CHARS.length]}${nameForIndex(
+      Math.floor(number / NAME_CHARS.length)
+    )}`;
+  }
+  return NAME_CHARS[number];
+};
+
+export const compressFile = (imageDef: ImageDefinition): ImageDefinition => {
+  const layerNames = getLayerNames(imageDef.shapes);
+  const vectorNames = getVectorNames(imageDef.shapes);
+
+  const result = layerNames.reduce<ImageDefinition>(
+    (result, name, index) => renameLayer(result, name, nameForIndex(index)),
+    imageDef
+  );
+
+  return vectorNames.reduce<ImageDefinition>(
+    (result, name, index) => renameVector(result, name, nameForIndex(index)),
+    result
+  );
+};
