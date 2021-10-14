db44d3054dba5518d0081641bb4ba1fbf43970c4
Matthijs Groen <matthijs.groen@gmail.com>
2021-10-14 23:41:39 +0200

    Update to parcel 2

---
+++ docs/beta/index.25038abd.js
@@ -0,0 +1,2 @@
+const e=e=>{const n=document.getElementById("errorBox"),t=document.createElement("p");t.textContent=e,n.appendChild(t)};window.addEventListener("error",(n=>{e(n.message),e(`${n.colno}`),e(`${n.lineno}`),e(`${n.error}`)}));
+//# sourceMappingURL=index.25038abd.js.map
