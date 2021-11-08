a6fb4efeaf07c3e8a3e77282d411b94bb4567967
Matthijs Groen <matthijs.groen@gmail.com>
2021-11-08 11:35:35 +0100

    Fix smoke

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -9,18 +9,18 @@ import HomepageFeatures from "../components/HomepageFeatures";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--primary button--lg"
             to="/docs/download"
           >
             Download
           </Link>
-          <Link className="button button--info button--lg" to="/docs/demo">
+          <Link className="button button--secondary button--lg" to="/docs/demo">
             Demo
           </Link>
         </div>
