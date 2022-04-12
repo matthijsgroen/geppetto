7a5e6cd80bf4fd278a7f0fa7c473c99b7ceaffb4
Matthijs Groen <matthijs.groen@gmail.com>
2022-04-13 00:28:49 +0200

    Start refactoring shaders

--- src/application/webgl/programs/showTexture.ts
+++ src/application/webgl/programs/showTexture.ts
@@ -1,41 +1,7 @@
 import { createProgram, WebGLRenderer } from "../lib/webgl";
-
-const textureVertexShader = `
-  attribute vec2 coordinates;
-  attribute vec2 aTextureCoord;
-  varying lowp vec2 vTextureCoord;
-  uniform vec4 scale;
-
-  void main() {
-    vTextureCoord = aTextureCoord.xy;
-    gl_Position = vec4((coordinates + scale.ba) * scale.y, 0.1, 1.0);
-  }
-`;
-
-const textureFragmentShader = `
-  precision mediump float;
-
-  varying mediump vec2 vTextureCoord;
-  uniform sampler2D uSampler;
-  uniform mediump vec2 uTextureDimensions;
-  uniform mediump vec3 backgroundColor;
-
-  void main(void) {
-    highp vec2 coord = vTextureCoord.xy / uTextureDimensions;
-    mediump vec4 texelColor = texture2D(uSampler, coord);
-
-    int xRow = int(mod(gl_FragCoord.x / 20.0, 2.0));
-    int yRow = int(mod(gl_FragCoord.y / 20.0, 2.0));
-    vec3 baseColor = backgroundColor;
-    if (xRow == 1 && yRow == 1 || xRow == 0 && yRow == 0) {
-      baseColor *= 0.9;
-    }
-
-    lowp vec4 backColor = vec4(baseColor, 1.0);
-
-    gl_FragColor = backColor * (1.0-texelColor.a) + vec4(texelColor.rgb * texelColor.a, texelColor.a);
-  }
-`;
+import raw from "raw.macro";
+const textureVertexShader = raw("./showTexture.vert");
+const textureFragmentShader = raw("./showTexture.frag");
 
 export const showTexture = (): {
   setImage(image: HTMLImageElement): void;
---
+++ src/application/webgl/programs/showTexture.vert
@@ -0,0 +1,10 @@
+
+  attribute vec2 coordinates;
+  attribute vec2 aTextureCoord;
+  varying lowp vec2 vTextureCoord;
+  uniform vec4 scale;
+
+  void main() {
+    vTextureCoord = aTextureCoord.xy;
+    gl_Position = vec4((coordinates + scale.ba) * scale.y, 0.1, 1.0);
+  }
