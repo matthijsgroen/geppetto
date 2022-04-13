706b3bb5d8cb51dee2c9533aadcc8b3bfb60b782
Matthijs Groen <matthijs.groen@gmail.com>
2022-04-13 19:52:13 +0200

    Setup new deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    echo "There are unstaged changes. Please make sure the tree is clean before making a release."
+    exit 1
+fi
+echo "Preparing site for deploy"
+rm -rf build/
+yarn build
+git switch gh-pages
+mkdir -p docs/app
+cp -rf build/* docs/app
+git add docs/app
+
+# git commit -m "Website deploy"
+echo "Site is ready for deploy"
+git status
+
+echo "use ./finish-app-deploy.sh to commit, push and switch branch back"
