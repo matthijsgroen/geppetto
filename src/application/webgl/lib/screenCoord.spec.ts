9e63f37de36959f7e1ee52a3915614ef1ae98ed3
Matthijs Groen <matthijs.groen@gmail.com>
2022-05-18 23:29:43 +0200

    add some conversion tests

--- src/application/webgl/lib/screenCoord.spec.ts
+++ src/application/webgl/lib/screenCoord.spec.ts
@@ -3,20 +3,71 @@ import { ScreenTranslation, Size } from "../../types";
 import { imageToPixels } from "./screenCoord";
 
 describe("imageToPixels", () => {
-  it("translates file coordinates to pixels on screen", () => {
-    const center: Vec2 = [0, 0];
-    const translation: ScreenTranslation = {
-      panX: 0,
-      panY: 0,
-      zoom: 1,
-      scale: 1,
-    };
-    const containerSize: Size = {
-      width: 500,
-      height: 300,
-    };
-    const result = imageToPixels(center, translation, containerSize);
+  it.each<{
+    translation: ScreenTranslation;
+    screenPos: Vec2;
+    expectedResult: Vec2;
+  }>([
+    {
+      translation: {
+        panX: 0,
+        panY: 0,
+        zoom: 1,
+        scale: 1,
+      },
+      screenPos: [0, 0],
+      expectedResult: [250, 150],
+    },
+    {
+      translation: {
+        panX: -0.5, // subtracts a quarter of 'width' from the center 250 - 125
+        panY: -0.5, // adds a quarter of 'height' to the center 150 + 75
+        zoom: 1,
+        scale: 1,
+      },
+      screenPos: [0, 0],
+      expectedResult: [125, 225],
+    },
+    {
+      translation: {
+        panX: 0,
+        panY: 0,
+        zoom: 1,
+        scale: 1, // one pixel in the image is one on the screen
+      },
+      screenPos: [50, 0],
+      expectedResult: [250 + 50, 150],
+    },
+    {
+      translation: {
+        panX: 0,
+        panY: 0,
+        zoom: 1,
+        scale: 0.5, // two pixel in the image is one on the screen
+      },
+      screenPos: [50, 0],
+      expectedResult: [250 + 25, 150],
+    },
+    {
+      translation: {
+        panX: 0.5,
+        panY: 0,
+        zoom: 2,
+        scale: 0.5, // two pixel in the image is one on the screen
+      },
+      screenPos: [50, 0],
+      expectedResult: [250 + 50 * 0.5 * 2 + 125 * 2, 150],
+    },
+  ])(
+    "translates file coordinates to pixels on screen",
+    ({ screenPos, expectedResult, translation }) => {
+      const containerSize: Size = {
+        width: 500,
+        height: 300,
+      };
+      const result = imageToPixels(screenPos, translation, containerSize);
 
-    expect(result).toEqual([250, 150]);
-  });
+      expect(result).toEqual(expectedResult);
+    }
+  );
 });
