76d3079b43fd113314f45a137b64416a9db02160
Matthijs Groen <matthijs.groen@gmail.com>
2022-05-25 20:28:05 +0200

    Improve tree diffing

--- src/application/treeEnvironments/useLayerTreeItems.tsx
+++ src/application/treeEnvironments/useLayerTreeItems.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useRef } from "react";
 import { iconMapping } from "../../animation/file2/mutation";
+import { newFile } from "../../animation/file2/new";
 import { GeppettoImage, NodeType } from "../../animation/file2/types";
 import {
   Icon,
@@ -17,21 +18,53 @@ export type LayerItem = TreeItem<
 export type ActionButton = "visibility";
 
 const populateTree = (
-  hierarchy: GeppettoImage["layerHierarchy"],
-  mutations: GeppettoImage["mutations"],
-  layers: GeppettoImage["layers"],
-  folders: GeppettoImage["layerFolders"],
+  newFile: GeppettoImage,
+  previousFile: GeppettoImage,
   result: Record<TreeItemIndex, LayerItem>,
   actionHandler: (nodeId: string, button: ActionButton) => void,
   showMutations: boolean,
   toggleVisibility: boolean
 ) => {
+  const hierarchy = newFile.layerHierarchy;
+
+  const changedNodes = Object.keys(newFile.layerHierarchy).filter((key) => {
+    const item = newFile.layerHierarchy[key];
+    if (item !== previousFile.layerHierarchy[key]) {
+      return true;
+    }
+    if (
+      item.type === "layer" &&
+      newFile.layers[key] !== previousFile.layers[key]
+    ) {
+      return true;
+    }
+    if (
+      item.type === "layerFolder" &&
+      newFile.layerFolders[key] !== previousFile.layerFolders[key]
+    ) {
+      return true;
+    }
+    if (
+      item.type === "mutation" &&
+      newFile.mutations[key] !== previousFile.mutations[key]
+    ) {
+      return true;
+    }
+
+    return false;
+  });
+
   const populateNode = (nodeId: string) => {
     const item = hierarchy[nodeId];
-
     const childIds = (item.children || []).filter(
       (childId) => showMutations || hierarchy[childId].type !== "mutation"
     );
+    if (!changedNodes.includes(nodeId)) {
+      for (const childId of childIds) {
+        populateNode(childId);
+      }
+      return;
+    }
 
     if (item.type === "root") {
       result[nodeId] = {
@@ -53,7 +86,7 @@ const populateTree = (
       return;
     }
     if (item.type === "layer") {
-      const layerData = layers[nodeId];
+      const layerData = newFile.layers[nodeId];
       const data: TreeData<NodeType> = {
         name: layerData.name,
         icon: "📄",
@@ -81,7 +114,7 @@ const populateTree = (
       };
     }
     if (item.type === "layerFolder") {
-      const layerFolderData = folders[nodeId];
+      const layerFolderData = newFile.layerFolders[nodeId];
       const data: TreeData<NodeType> = {
         name: layerFolderData.name,
         icon: "📁",
@@ -109,7 +142,7 @@ const populateTree = (
       };
     }
     if (item.type === "mutation") {
-      const mutationData = mutations[nodeId];
+      const mutationData = newFile.mutations[nodeId];
       result[nodeId] = {
         index: nodeId,
         canMove: true,
@@ -138,26 +171,18 @@ export const useLayerTreeItems = (
   toggleVisibility: boolean
 ) => {
   const treeItemsRef = useRef<Record<TreeItemIndex, LayerItem>>({});
+  const fileRef = useRef<GeppettoImage>(newFile());
 
   useMemo(() => {
     populateTree(
-      file.layerHierarchy,
-      file.mutations,
-      file.layers,
-      file.layerFolders,
+      file,
+      fileRef.current,
       treeItemsRef.current,
       actionHandler,
       showMutations,
       toggleVisibility
     );
-  }, [
-    file.layerHierarchy,
-    file.mutations,
-    file.layers,
-    file.layerFolders,
-    toggleVisibility,
-    showMutations,
-    actionHandler,
-  ]);
+    fileRef.current = file;
+  }, [file, toggleVisibility, showMutations, actionHandler]);
   return treeItemsRef.current;
 };
