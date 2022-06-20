c108614816c944fe226a74647de9b53574b66804
Matthijs Groen <matthijs.groen@gmail.com>
2022-06-20 21:37:12 +0200

    improve visual update speed of slider

--- src/application/composition/ControlEdit.tsx
+++ src/application/composition/ControlEdit.tsx
@@ -1,7 +1,8 @@
 import produce from "immer";
-import { ChangeEvent, useCallback, useTransition } from "react";
+import { ChangeEvent, useEffect, useState, useTransition } from "react";
 import { Control, ControlPanel, Icon, Title } from "../../ui-components";
 import { useFile } from "../applicationMenu/FileContext";
+import useEvent from "../hooks/useEvent";
 
 type ControlEditProps = {
   selectedControlIds: string[];
@@ -17,26 +18,32 @@ export const ControlEdit: React.FC<ControlEditProps> = ({
     activeControlId !== null ? file.controlHierarchy[activeControlId] : null;
   const [, startTransition] = useTransition();
 
-  const onChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      if (activeControlId === null) return;
-      const value = e.currentTarget.valueAsNumber;
-      startTransition(() => {
-        setFile(
-          produce((draft) => {
-            draft.controlValues[activeControlId] = value;
-          })
-        );
-      });
-    },
-    [setFile, activeControlId]
-  );
-
-  if (
+  const isNoControl =
     activeControlId === null ||
     !hierarchyItem ||
-    hierarchyItem.type !== "control"
-  ) {
+    hierarchyItem.type !== "control";
+
+  const controlValue = isNoControl ? 0 : file.controlValues[activeControlId];
+  const [slideValue, setSlideValue] = useState(controlValue);
+  useEffect(() => {
+    if (activeControlId) {
+      setSlideValue(controlValue);
+    }
+  }, [activeControlId, controlValue]);
+  const onChange = useEvent((e: ChangeEvent<HTMLInputElement>) => {
+    if (activeControlId === null) return;
+    const value = e.currentTarget.valueAsNumber;
+    setSlideValue(value);
+    startTransition(() => {
+      setFile(
+        produce((draft) => {
+          draft.controlValues[activeControlId] = value;
+        })
+      );
+    });
+  });
+
+  if (isNoControl) {
     return null;
   }
   const control = file.controls[activeControlId];
@@ -52,10 +59,10 @@ export const ControlEdit: React.FC<ControlEditProps> = ({
             min={0}
             max={control.steps.length - 1}
             step={0.01}
-            value={file.controlValues[activeControlId]}
+            value={slideValue}
             onChange={onChange}
           />
-          <p>{file.controlValues[activeControlId]}</p>
+          <p>{slideValue}</p>
         </Control>
       </ControlPanel>
     </>
