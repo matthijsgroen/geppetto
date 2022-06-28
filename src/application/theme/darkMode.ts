31bbb180edee66c1a92a5f621677f6ca12285953
Matthijs Groen <matthijs.groen@gmail.com>
2022-06-28 13:04:39 +0200

    Update theme color

--- src/application/theme/darkMode.ts
+++ src/application/theme/darkMode.ts
@@ -6,6 +6,13 @@ const colorSchemeListener = ({
   matches,
 }: MediaQueryListEvent | MediaQueryList) => {
   colorScheme.darkMode = matches;
+
+  const themeColor = document.querySelector("meta[name='theme-color']");
+  if (matches) {
+    themeColor?.setAttribute("content", "#35363a");
+  } else {
+    themeColor?.setAttribute("content", "#e7eaed");
+  }
 };
 
 const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
