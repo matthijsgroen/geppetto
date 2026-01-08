0b2b404ea5e63a403f55c7797c3f10d805d0a0f7
Matthijs Groen <matthijs.groen@gmail.com>
2026-01-08 14:36:53 +0100

    Add measuring coverage

--- packages/player/vitest.config.ts
+++ packages/player/vitest.config.ts
@@ -4,5 +4,20 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "json-summary", "html"],
+      exclude: [
+        "node_modules/",
+        "**/*.d.ts",
+        "**/*.config.*",
+      ],
+      thresholds: {
+        lines: 77,
+        branches: 65,
+        functions: 66,
+        statements: 78,
+      },
+    },
   },
 });
