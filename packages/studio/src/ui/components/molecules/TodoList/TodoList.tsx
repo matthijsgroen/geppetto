cfeea6ba2d1e799beca6c6aff5b135965dd4d869
Matthijs Groen <matthijs.groen@gmail.com>
2026-01-14 14:19:51 +0100

    Fix todo markup

--- packages/studio/src/ui/components/molecules/TodoList/TodoList.tsx
+++ packages/studio/src/ui/components/molecules/TodoList/TodoList.tsx
@@ -1,5 +1,5 @@
 import type { FC, PropsWithChildren } from "react";
 
 export const TodoList: FC<PropsWithChildren> = ({ children }) => (
-  <ul className="my-4 ml-6 space-y-1">{children}</ul>
+  <ul className="my-4 ml-6 flex flex-col space-y-1">{children}</ul>
 );
